6b6339154f72f2aca12bb300db773ee0a5280716
Latera <laternigatu202@gmail.com>
2024-05-23 14:47:21 +0300

    appliedSolidity

---
+++ AppliedSolidity/Inheritance/virtualOverride.sol
@@ -0,0 +1,14 @@
+import "./Hero.sol";
+
+contract Warrior is Hero {
+    function attack(Enemy enemy) external override {
+        enemy.takeAttack(AttackTypes.Brawl);
+    }
+}
+
+
+contract Mage is Hero {
+    function attack(Enemy enemy) external override {
+        enemy.takeAttack(AttackTypes.Spell);
+    }
+}
